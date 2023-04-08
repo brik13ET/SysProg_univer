4f84bfe77fe1b56e74b797aab116b590ecf357a4
brik13ET <aleev.ibragim0909@gmail.com>
2023-04-08 07:47:28 +0400

    lexer fin

---
+++ SysProg_univer/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SysProg_univer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox1.Font = new System.Drawing.Font("Lucida Console", 14.25F);
+			this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(860, 241);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.TabStop = false;
+			this.richTextBox1.Text = "";
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(3, 250);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(860, 38);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Анализировать";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.richTextBox2);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.button1);
+			this.splitContainer1.Size = new System.Drawing.Size(866, 590);
+			this.splitContainer1.SplitterDistance = 295;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.AcceptsTab = true;
+			this.richTextBox2.BulletIndent = 4;
+			this.richTextBox2.DetectUrls = false;
+			this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox2.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.richTextBox2.HideSelection = false;
+			this.richTextBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+			this.richTextBox2.Size = new System.Drawing.Size(866, 295);
+			this.richTextBox2.TabIndex = 0;
+			this.richTextBox2.TabStop = false;
+			this.richTextBox2.Text = "do\n{\n\tmyvar1 = myvar2 + 3;\n\tmyvar2 += 3;\n\tmyvar42 = 15 - 11 + 2;\n\tmyvar3 -= myvar" +
+    "1 - myvar42 + myvar2 - 15;\n} while ( myvar2 != myvar3 \n";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(866, 590);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "Form1";
+			this.Text = "Анализатор";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+	}
+}
+
